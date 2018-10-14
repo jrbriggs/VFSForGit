95c31e016fe97f79bf344c0c6b449e9ced652d7f
John Briggs <jrbriggs@gmail.com>
2018-10-14 13:39:36 -0400

    Write a unified product install script to GVFS.Build output dir

--- Scripts/StageTestArtifacts.bat
+++ Scripts/StageTestArtifacts.bat
@@ -11,11 +11,13 @@ IF EXIST %VFS_STAGEDIR% (
   rmdir /s /q %VFS_STAGEDIR%
 )
 mkdir %VFS_STAGEDIR%\src\Scripts
+mkdir %VFS_STAGEDIR%\BuildOutput\GVFS.Build
 mkdir %VFS_STAGEDIR%\BuildOutput\GVFS.FunctionalTests\bin\x64\%Configuration%\netcoreapp2.1
 mkdir %VFS_STAGEDIR%\BuildOutput\GVFS.FunctionalTests.Windows\bin\x64\%Configuration%\
 
 REM Make a minimal 'test' enlistment to pass along our pipeline.
 copy %VFS_SRCDIR%\init.cmd %VFS_STAGEDIR%\src\
 copy %VFS_SCRIPTSDIR%\*.* %VFS_STAGEDIR%\src\Scripts\
+copy %VFS_OUTPUTDIR%\GVFS.Build\*.* %VFS_STAGEDIR%\BuildOutput\GVFS.Build
 copy %VFS_OUTPUTDIR%\GVFS.FunctionalTests\bin\x64\%Configuration%\netcoreapp2.1\*.* %VFS_STAGEDIR%\BuildOutput\GVFS.FunctionalTests\bin\x64\%Configuration%\netcoreapp2.1\
 copy %VFS_OUTPUTDIR%\GVFS.FunctionalTests.Windows\bin\x64\%Configuration%\*.* %VFS_STAGEDIR%\BuildOutput\GVFS.FunctionalTests.Windows\bin\x64\%Configuration%\
